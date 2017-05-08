e9eef6b9feb6708593bfb35b52d0feebca75161c
ALYSSA EVANS <ALYSSA.M.EVANS@GMAIL.COM>
2017-05-08 15:21:05 -0700

    added readme with links to repo, deployed site, and pivotal tracker. working on edit page with joins

---
+++ README.md
@@ -0,0 +1,3 @@
+repo: https://github.com/missalyss/midterm-galvanize-reads
+deployed site: https://vast-depths-75601.herokuapp.com/
+pivotal tracker: https://www.pivotaltracker.com/projects/2018917
--- routes/books.js
+++ routes/books.js
@@ -15,12 +15,19 @@ router.get('/', (req, res, next) => {
   })
 })
 
+// Render edit page for books
 router.get('/edit/:id', (req, res) => {
-  knex('books').where('id', req.params.id).then((thisBook) => {
+  knex.select('books.id as books_id', 'books.title', 'books.genre', 'books.description', 'books.cover_url', 'authors.id as authors_id', 'authors.first_name', 'authors.last_name')
+  .table('books')
+  .where('books.id', req.params.id)
+  .innerJoin('authors_books', 'books.id', 'authors_books.book_id')
+  .innerJoin('authors', 'authors_books.author_id', 'authors.id')
+  .then((thisBook) => {
     res.render('books/edit', {thisBook})
   })
 })
 
+// Render delete page for books
 router.get('/delete/:id', (req, res) => {
   var thisId = req.params.id
   res.render('books/delete', {thisId})
@@ -32,7 +39,11 @@ router.get('/new', (req, res) => {
 
 router.get('/:id', (req, res) => {
   var id = req.params.id
-  knex('books').where({id}).then((thisBook) => {
+  knex.select('books.id as books_id', 'books.title', 'books.genre', 'books.description', 'books.cover_url', 'authors.id as authors_id', 'authors.first_name', 'authors.last_name')
+  .table('books')
+  .where('books.id', req.params.id)
+  .innerJoin('authors_books', 'books.id', 'authors_books.book_id')
+  .innerJoin('authors', 'authors_books.author_id', 'authors.id').then((thisBook) => {
     res.render('books/show-one', {thisBook})
   })
 })

eb0dee1823186e21bb3e1b53a0b940d4725ba9c9
ALYSSA EVANS <ALYSSA.M.EVANS@GMAIL.COM>
2017-05-08 09:41:11 -0700

    migration tables created

--- db/migrations/20170508092750_books_create.js
+++ db/migrations/20170508092750_books_create.js
@@ -1,8 +1,15 @@
 
-exports.up = function(knex, Promise) {
-  return knex.schema.createTable()
-};
+exports.up = function (knex, Promise) {
+  return knex.schema.createTable('books', (t) => {
+    t.increments()
+    t.string('title').notNullable()
+    t.string('genre')
+    t.string('description')
+    t.text('cover_url')
+    t.timestamps(true, true)
+  })
+}
 
-exports.down = function(knex, Promise) {
-
-};
+exports.down = function (knex, Promise) {
+  return knex.schema.dropTable('books')
+}

46bce7268d796c482e894bc089b15c4cdd08267e
ALYSSA EVANS <ALYSSA.M.EVANS@GMAIL.COM>
2017-05-08 16:05:55 -0700

    books post now adding authors' info to authors_books

--- db/seeds/1_books.js
+++ db/seeds/1_books.js
@@ -12,14 +12,14 @@ exports.seed = function (knex, Promise) {
         },
         {
           id: 2,
-          title: 'You Don\'t Know JS: ES6 & BeyondThink Python',
+          title: 'Think Python',
           genre: 'Python',
           description: 'If you want to learn how to program, working with Python is an excellent way to start. This hands-on guide takes you through the language a step at a time, beginning with basic programming concepts before moving on to functions, recursion, data structures, and object-oriented design. This second edition and its supporting code have been updated for Python 3.',
           cover_url: 'https://s3-us-west-2.amazonaws.com/assessment-images/galvanize_reads/photos/think_python.jpg'
         },
         {
           id: 3,
-          title: 'You Don\'t Know JS: ES6 & BeyondLearning React Native',
+          title: 'Learning React Native',
           genre: 'JavaScript',
           description: 'Get a practical introduction to React Native, the JavaScript framework for writing and deploying fully featured mobile apps that look and feel native. With this hands-on guide, you’ll learn how to build applications that target iOS, Android, and other mobile platforms instead of browsers. You’ll also discover how to access platform features such as the camera, user location, and local storage.',
           cover_url: 'https://s3-us-west-2.amazonaws.com/assessment-images/galvanize_reads/photos/learning_react_native.jpg'
--- routes/books.js
+++ routes/books.js
@@ -34,7 +34,9 @@ router.get('/delete/:id', (req, res) => {
 })
 
 router.get('/new', (req, res) => {
-  res.render('books/new')
+  knex('authors').then((allAuthors) => {
+    res.render('books/new', {allAuthors})
+  })
 })
 
 router.get('/:id', (req, res) => {
@@ -50,9 +52,18 @@ router.get('/:id', (req, res) => {
 
 // Other routes
 router.post('/api', (req, res) => {
-  var newBook = { title, genre, description, cover_url } = req.body
-  knex('books').insert(newBook).then(() => {
-    res.redirect('/books')
+  var newBook = { title: req.body.title, genre: req.body.genre, description: req.body.description, cover_url: req.body.cover_url }
+  var theAuthors = { author_id: parseInt(req.body.anAuthor) }
+  console.log(theAuthors)
+
+  knex('books').insert(newBook, '*').then((thisBook) => {
+    console.log(thisBook)
+    var newBookId = thisBook[0].id
+    theAuthors.book_id = newBookId
+    console.log(theAuthors)
+    knex('authors_books').insert(theAuthors).then(() => {
+      res.redirect('/books')
+    })
   })
 })
 
